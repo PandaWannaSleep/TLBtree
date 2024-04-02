20f569aa4ee9342028116b0a975b05cc71919d21
PandaWannaSleep <jikangjiangcs@126.com>
2024-04-02 22:10:27 +0800

    datagen improve

--- Concurrent/test/datagen.cc
+++ Concurrent/test/datagen.cc
@@ -15,20 +15,22 @@ using std::string;
 using std::ofstream;
 using std::ifstream;
 
-enum DistributionType {RAND = 0, ZIPFIAN};
-
-struct WorkloadType {
+struct WorkloadType
+{
+    //the operations times for the workload
     int operations = MILLION;
+    // ratio for read, insert, update, remove in workload.
     float read = 1.0;
     float insert = 0;
     float update = 0;
     float remove = 0;
     DistributionType dist = RAND;
+    //skewness for zipfian distribution
     float skewness = 0.8;
-    bool valid() {
+    bool valid(){   
         return read + insert + update + remove == 1.0 && skewness > 0 && skewness < 1.0;
     }
-    void print() {
+    void print(){
         cout << "=========WORKLOAD TYPE=========" << endl;
         cout << "Operations  : " << operations << endl;
         cout << "Read Ratio  : " << read << endl;
@@ -43,13 +45,14 @@ struct WorkloadType {
     }
 };
 
-class OperationGenerator {
+class OperationGenerator{
 public :
     OperationType mappings_[100];
     std::default_random_engine gen_;
     std::uniform_int_distribution<uint32_t> dist_;
 
-    OperationGenerator(WorkloadType &w) : gen_(getRandom()) {
+    //maps the input random num to 1-100(partitioned by CRUD ration)
+    OperationGenerator(WorkloadType &w) : gen_(getRandom()){
         int read_end = 100 * w.read;
         int insert_end = read_end + 100 * w.insert;
         int update_end = insert_end + 100 * w.update;
@@ -57,7 +60,7 @@ public :
 
         for(int i = 0; i < read_end; i++)
             mappings_[i] = OperationType::READ;
-
+        
         for(int i = read_end; i < insert_end; i++)
             mappings_[i] = OperationType::INSERT;
 
@@ -66,18 +69,17 @@ public :
 
         for(int i = update_end; i < remove_end; i++)
             mappings_[i] = OperationType::DELETE;
-    }
 
+    }
+    
     OperationType next() {
         return mappings_[dist_(gen_) % 100];
     }
 };
 
-// try not to generate duplicate keys, because some tree indices may not get used to it
-
-void gen_dataset(int64_t *arr, uint64_t scale, bool random) {
+void gen_dataset(int64_t *arr, uint64_t scale, DatasetType type){
     std::mt19937 gen(10007);
-    if(random) {
+    if(type == UNIFORM) {
         uint64_t step = MAX_KEY / scale;
         std::uniform_int_distribution<int64_t> dist(0, MAX_KEY);
         for(int64_t i = 0; i < scale; i++) {
@@ -88,11 +90,29 @@ void gen_dataset(int64_t *arr, uint64_t scale, bool random) {
             #endif
         }
         std::shuffle(arr, arr + scale - 1, gen);
-    } else {
+    } else if(type == NORMAL){
         std::normal_distribution<double> dist(MAX_KEY / 2, MAX_KEY / 8);
         int64_t i = 0;
         while (i < scale) {
             double val = (uint64_t)dist(gen);
+            if(val < 0 || val > (double) MAX_KEY) {
+                continue;
+            } else {
+                arr[i++] = (int64_t)std::round(val);
+            }
+        }
+    }
+    else if(type == LOGNORMAL){
+        double mu = 0.0; // 均值
+        double sigma = 1.0; // 标准差
+        double max_value = MAX_KEY;
+        double min_value = 0.0;
+        double scaling_factor = (max_value - min_value) / (1.0 - std::exp(-mu + sigma * sigma / 2.0));
+        double shift = min_value - scaling_factor * std::exp(mu - sigma * sigma / 2.0);
+        std::lognormal_distribution<double> dist(mu, sigma);
+        int64_t i = 0;
+        while (i < scale) {
+            double val = scaling_factor * dist(gen) + shift;
             if(val < 0 || val > (double) MAX_KEY) {
                 continue;
             } else {
@@ -120,14 +140,15 @@ void gen_workload(int64_t *arr, uint64_t scale, QueryType * querys, WorkloadType
 }
 
 int main(int argc, char ** argv) {
-    static const bool DATASET_RANDOM = true;
-    bool opt_zipfian  = false;
+    // static const bool DATASET_RANDOM = false;
+    DatasetType dstype = UNIFORM;
+    bool opt_zipfian = false;
     WorkloadType w;
 
-    static const char * optstr = "r:i:u:d:o:s:hz"; 
+    static const char * optstr = "g:r:i:u:d:o:s:hz"; 
     opterr = 0;
     char opt;
-    while((opt = getopt(argc, argv, optstr)) != -1) {
+    while((opt = getopt(argc, argv, optstr)) != -1){
         switch(opt) {
         case 'o':
             w.operations = atoi(optarg) * MILLION;
@@ -150,6 +171,9 @@ int main(int argc, char ** argv) {
         case 'z':
             opt_zipfian = true;
             break;
+        case 'g':
+            dstype = DatasetType(atoi(optarg));
+            break;
         case '?':
         case 'h':
         default:
@@ -162,6 +186,7 @@ int main(int argc, char ** argv) {
             cout << "\t -i: " << "Insert ratio" << endl;
             cout << "\t -u: " << "update ratio" << endl;
             cout << "\t -d: " << "Delete ratio" << endl;
+            cout << "\t -g: " << "The distribution of generated dataset: uniform(0), normal(1), lognormal(2)"<<endl;
             exit(-1);
         }
     }
@@ -170,11 +195,10 @@ int main(int argc, char ** argv) {
         cout << "Invalid workload configuration" << endl;
         exit(-1);
     }
+
     if(opt_zipfian == true) {
         w.dist = ZIPFIAN;
     }
-
-    w.print();
 
     #ifdef DEBUG
         uint64_t scale = LOADSCALE * KILO;
@@ -184,20 +208,39 @@ int main(int argc, char ** argv) {
 
     int64_t * arr = new int64_t[scale];
     if(!file_exist("dataset.dat")) {
-        gen_dataset(arr, scale, DATASET_RANDOM);
+        gen_dataset(arr, scale, dstype);
         ofstream fout("dataset.dat", std::ios::binary);
         fout.write((char *) arr, sizeof(int64_t) * scale);
         fout.close();
+        cout << "=========DATASET TYPE=========" << endl;
+        cout << "number      : " << scale << endl;
+        cout << "Distribution: " ;
+        switch (dstype)
+        {
+        case UNIFORM:
+            cout<<"uniform"<<endl;
+            break;
+        case NORMAL:
+            cout<<"normal"<<endl;
+            break;
+        case LOGNORMAL:
+            cout<<"lognormal"<<endl;
+            break;
+        default:
+            break;
+        }
+        cout << "===============================" << endl;
         cout << "generate a dataset file" << endl;
     } else {
         ifstream fin("dataset.dat", std::ios::binary);
         fin.read((char *)arr, sizeof(int64_t) * scale);
         fin.close();
     }
-
+    
+    w.print();
     QueryType * querys = new QueryType[w.operations];
     gen_workload(arr, scale, querys, w);
-    
+
     ofstream fout("workload.txt");
     for(int i = 0; i < w.operations; i++) {
         fout << querys[i].op << " " << querys[i].key << endl;
@@ -209,3 +252,6 @@ int main(int argc, char ** argv) {
     delete [] querys;
     return 0;
 }
+
+
+
